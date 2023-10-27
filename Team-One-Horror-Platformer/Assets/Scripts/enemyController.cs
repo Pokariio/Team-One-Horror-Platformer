2ea92a1e5316d1af2bc1a388cf6e2b67b6949cea
Hanaya03 <ha4863519@gmail.com>
2023-10-27 03:13:27 -0700

    removed a* and used unity navmesh for navigation

--- Team-One-Horror-Platformer/Assets/Scripts/enemyController.cs
+++ Team-One-Horror-Platformer/Assets/Scripts/enemyController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyController : MonoBehaviour
 {
+    public Transform playerTransform;
+
+    public NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,6 @@ public class enemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(playerTransform.position);
     }
 }
